f4fb2e243568de534998095d235fe2626a130764
yaratavares <yaracriistavares@gmail.com>
2022-01-27 15:09:00 -0300

    user authentication with jsonwebtoken

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import prismaClient from "../prisma"
+import {sign} from "jsonwebtoken"
 
 interface IAccessTokenResponse {
     access_token: string
@@ -30,8 +32,37 @@ class AuthenticateUserService {
             }
         })
         const { login, id, avatar_url, name} = response.data;
+        let user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        })
 
-        return response.data;
+        if(!user){
+            await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
+        }
+        
+        const token = sign({
+            user: {
+                name: user.name,
+                avatar_url: user.avatar_url,
+                id: user.id
+            }
+        }, process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        )
+
+        return {token, user};
     }
 
 }
