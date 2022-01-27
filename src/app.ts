1a5a1f03d632402e2dd55d8641a2f742e4437dbf
yaratavares <yaracriistavares@gmail.com>
2022-01-27 14:48:18 -0300

    config github oauth

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express, {json} from "express";
+import { router } from "./routes";
+
+const app = express();
+app.use(json())
+app.use(router);
+
+app.get("/github", (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+})
+
+app.get("/signin/callback", (request, response) => {
+    const { code } = request.query;
+    return response.json(code)
+})
+
+app.listen(4000, () => console.log("Rodando na porta 4000"));
